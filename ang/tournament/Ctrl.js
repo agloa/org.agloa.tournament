23ffd91d951c287ba3fd1c268d3d9a50c4a943e4
Mike Steigerwald <CIO@AGLOA.org>
2020-06-01 17:43:45 -0700

    get all contact data

--- ang/tournament/Ctrl.js
+++ ang/tournament/Ctrl.js
@@ -7,10 +7,10 @@
 
         // If you need to look up data when opening the page, list it out under "resolve".
         resolve: {
-          myContact: function(crmApi) {
+          $ctrl: function(crmApi) {
             return crmApi('Contact', 'getsingle', {
               id: 'user_contact_id',
-              return: ['first_name', 'last_name', 'display_name']
+              return: ['first_name', 'last_name', 'middle_name', 'prefix_id', 'suffix_id', 'gender_id', 'display_name', 'birth_date', 'email', 'modified_date']
             });
           }
         }
@@ -21,14 +21,14 @@
   // The controller uses *injection*. This default injects a few things:
   //   $scope -- This is the set of variables shared between JS and HTML.
   //   crmApi, crmStatus, crmUiHelp -- These are services provided by civicrm-core.
-  //   myContact -- The current contact, defined above in config().
-  angular.module('tournament').controller('TournamentCtrl', function($scope, crmApi, crmStatus, crmUiHelp, myContact) {
+  //   $ctrl -- The current contact, defined above in config().
+  angular.module('tournament').controller('TournamentCtrl', function($scope, crmApi, crmStatus, crmUiHelp, $ctrl) {
     // The ts() and hs() functions help load strings for this module.
     var ts = $scope.ts = CRM.ts('tournament');
     var hs = $scope.hs = crmUiHelp({file: 'CRM/tournament/Ctrl'}); // See: templates/CRM/tournament/Ctrl.hlp
 
-    // We have myContact available in JS. We also want to reference it in HTML.
-    $scope.myContact = myContact;
+    // We have $ctrl available in JS. We also want to reference it in HTML.
+    $scope.$ctrl = $ctrl;
 
     $scope.save = function save() {
       return crmStatus(
@@ -36,9 +36,9 @@
         {start: ts('Saving...'), success: ts('Saved')},
         // The save action. Note that crmApi() returns a promise.
         crmApi('Contact', 'create', {
-          id: myContact.id,
-          first_name: myContact.first_name,
-          last_name: myContact.last_name
+          id: $ctrl.id,
+          first_name: $ctrl.first_name,
+          last_name: $ctrl.last_name
         })
       );
     };
