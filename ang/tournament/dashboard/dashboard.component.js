4863579b91218597bf0b8e7dc02dc30acba908be
Mike Steigerwald <CIO@AGLOA.org>
2020-06-27 14:13:03 -0700

    use crmApi to set self.user

--- ang/tournament/dashboard/dashboard.component.js
+++ ang/tournament/dashboard/dashboard.component.js
@@ -1,19 +1,16 @@
 angular.module('tournament').component('dashboard', {
   templateUrl: '~/tournament/dashboard/dashboard.template.html',
-  controller: ['$routeParams', //'User',
-    function DashboardController($routeParams) { // , User
+  controller: ['$routeParams', 'crmApi', 'person',
+    function DashboardController($routeParams, crmApi, person) {
       var self = this;
       self.personArrowClass = "arrowRight";
-      self.userId = 1003; // TODO: logged in user from session
+      self.userId = 2; // TODO: logged in user from session
       
       // e.g., dashboard/43
       if ($routeParams.userId !== undefined) self.userId = $routeParams.userId;
 
       // Don't include 'child' form until user requests it.
       self.includePersonForm = false;
-
-      // User service returns additional data for userId
-      self.user = {displayName: "Mike Steigerwald", lastUpdatedDate: "06/13/2019"};// User.get({userId:self.userId});
 
       self.togglePersonVisibility = function togglePersonVisibility() {
         this.includePersonForm = !this.includePersonForm;
@@ -23,6 +20,36 @@ angular.module('tournament').component('dashboard', {
           this.personArrowClass = "arrowRight";
         }
       }
+
+      // User service returns additional data for userId
+      self.user = crmApi('Contact', 'getsingle', {
+        "sequential": 1,
+        "return": ["display_name","modified_date"],
+        "id": self.userId
+        }).then(
+            // Success
+            function(result) { 
+                self.user = result;
+            },
+            // Failure
+            function (error) { 
+                CRM.alert(ts('Could not get user record ID of %1, error = %2', {1: userId, 2: error}),ts('Not Found'),'error'); 
+            }
+        );
     }
   ]
 });
+
+angular.module('tournament').factory('person', function ($q, crmApi) {
+  return{
+      // Get an individual contact record
+      // @param id Contact id  (per APIv3)
+      // @return Promise Contact (per APIv3)
+      get: function get(id) {
+          return crmApi('Contact', 'getsingle', {
+            "return": ["sort_name","display_name","last_name","first_name","middle_name","prefix_id","suffix_id","gender_id","birth_date"],
+            "id": id
+        });
+      }
+    };
+  });
