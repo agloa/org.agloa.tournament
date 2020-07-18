a2d5e194babdf6608c7091b210aad797790f70d4
Mike Steigerwald <Steiger@UMich.edu>
2020-07-18 15:19:04 -0500

    update basePath in karma

--- ang/karma.conf.js
+++ ang/karma.conf.js
@@ -2,17 +2,17 @@
 module.exports = function(config) {
   config.set({
 
-    basePath: './app',
+    basePath: './',
 
     files: [
-      'lib/angular/angular.js',
-      'lib/angular-animate/angular-animate.js',
-      'lib/angular-resource/angular-resource.js',
-      'lib/angular-route/angular-route.js',
-      '../node_modules/angular-mocks/angular-mocks.js',
+      'app/lib/angular/angular.js',
+      'app/lib/angular-animate/angular-animate.js',
+      'app/lib/angular-resource/angular-resource.js',
+      'app/lib/angular-route/angular-route.js',
+      'node_modules/angular-mocks/angular-mocks.js',
       '**/*.module.js',
       '*!(.module|.spec).js',
-      '!(lib)/**/*!(.module|.spec).js',
+      '!(app/lib)/**/*!(.module|.spec).js',
       '**/*.spec.js'
     ],
 
