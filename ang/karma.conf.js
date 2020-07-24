ca84fdffddec4c2dc1978fd806e8edc9ac410d6f
Mike Steigerwald <Steiger@UMich.edu>
2020-07-24 14:48:16 -0500

    move services to own folder

--- ang/karma.conf.js
+++ ang/karma.conf.js
@@ -5,15 +5,23 @@ module.exports = function(config) {
     basePath: './',
 
     files: [
+      'app/lib/jquery/dist/jquery.js',
       'app/lib/angular/angular.js',
-      'app/lib/angular-animate/angular-animate.js',
       'app/lib/angular-resource/angular-resource.js',
       'app/lib/angular-route/angular-route.js',
       'node_modules/angular-mocks/angular-mocks.js',
+      'CRM.js',
+      'Common.js',
+      'crmUtil.js',
+      'app.js',
       '**/*.module.js',
+      '**/*.service.js',
       '*!(.module|.spec).js',
       '!(app/lib)/**/*!(.module|.spec).js',
-      '**/*.spec.js'
+      '**/*.spec.js',
+      
+  // <script src="app/lib/jquery/dist/jquery.js"></script>
+  // <script src="tournament/dashboard/dashboard.component.js"></script>
     ],
 
     autoWatch: true,
