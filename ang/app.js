be60440a1da0d7046fe504f1a41caee5ef01ce21
Mike Steigerwald <CIO@AGLOA.org>
2020-07-25 11:58:03 -0700

    fix indentation

--- ang/app.js
+++ ang/app.js
@@ -5,26 +5,29 @@
 
   angular.module('tournament').config(['$routeProvider', function($routeProvider) {
 
-  // Configure the module route(s)
-  // Use this method to configure services by injecting their providers, e.g. for adding routes to the $routeProvider.
-  $routeProvider
-  .when('/tournament/dashboard/:userId?', {
-    template: '<dashboard></dashboard>'
-  })
-  .when('/tournament/person/:personId', {
-    template: '<person></person>'
-  })
-  .when('/tournament/address/:addressId', {
-    template: '<address></address>'
-  })
-  .when('/tournament/person/address/:personId', {
-    template: '<address></address>'
-  })
-  .when('/tournament/phone/:phoneId', {
-    template: '<phone></phone>'
-  }).otherwise( {
-    template: '<dashboard></dashboard>'
-  })
-}]);
+    // Configure the module route(s)
+    // Use this method to configure services by injecting their providers, e.g. for adding routes to the $routeProvider.
+    $routeProvider
+    .when('/tournament/dashboard/:userId?', {
+        template: '<dashboard></dashboard>'
+    })
+    .when('/tournament/person/:personId', {
+        template: '<person></person>'
+    })
+    .when('/tournament/address/:addressId', {
+        template: '<address></address>'
+    })
+    .when('/tournament/person/address/:personId', {
+        template: '<address></address>'
+    })
+    .when('/tournament/phone/:phoneId', {
+        template: '<phone></phone>'
+    })
+    .when('/tournament/phone/:phoneId', {
+        template: '<dashboard></dashboard>'
+    }).otherwise( {
+        template: '<dashboard></dashboard>'
+    })
+  }]);
 
 })(angular);
