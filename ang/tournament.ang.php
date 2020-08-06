ab0944136c8cf40e94a3ccc6cee728c55c7e4ea4
Mike Steigerwald <Steiger@UMich.edu>
2020-08-06 17:11:50 -0500

    begin CRM injection

--- ang/tournament.ang.php
+++ ang/tournament.ang.php
@@ -1,6 +1,5 @@
 <?php
-// This file declares an Angular module which can be autoloaded
-// in CiviCRM. See also:
+// This file declares an Angular module which can be autoloaded in CiviCRM. See also:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
 
 return array (
@@ -20,6 +19,5 @@ return array (
         0 => 'ang/tournament',
     ),
   'settings' => 
-    array (
-    ),
+    array (),
 );
