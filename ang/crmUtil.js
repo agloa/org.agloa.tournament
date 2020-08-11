29b0e52076402784aa3db5761415fe804402c014
Mike Steigerwald <Steiger@UMich.edu>
2020-08-11 15:41:11 -0500

    simplify crmApi

--- ang/crmUtil.js
+++ ang/crmUtil.js
@@ -8,68 +8,33 @@ CRM.alert = (message, title, severity) => {
 }
 
 angular.module('tournament').factory('crmApi', function($q) {
-    var crmApi = function(entity, action, params, message) {
-      // JSON serialization in CRM.api3 is not aware of Angular metadata like $$hash, so use angular.toJson()
-      var deferred = $q.defer();
-      var p;
-      
-      if (params && params.body_html) {
-        // CRM-18474 - remove Unicode Character 'LINE SEPARATOR' (U+2028)
-        // and 'PARAGRAPH SEPARATOR' (U+2029) from the html if present.
-        params.body_html = params.body_html.replace(/([\u2028]|[\u2029])/g, '\n');
-      }
+    return function(entity, action, params, message) {
 
-      p = new Promise(function(resolve, reject) {
+      return new Promise(function(resolve, reject) {
         if (typeof entity === 'string') {
-          $.post(CRM.url('civicrm/ajax/api4/' + entity + '/' + action), {
-            params: JSON.stringify(params),
-            index: index
-          })
-            .done(function (data) {
-              resolve(arrayObject(data));
-            })
-            .fail(function (data) {
-              reject(data.responseJSON);
-            });
+          entityType = entity;
         } else {
-          $.post(CRM.url('civicrm/ajax/api4'), {
-            calls: JSON.stringify(entity)
-          })
-            .done(function(data) {
-              _.each(data, function(item, key) {
-                data[key] = arrayObject(item);
-              });
-              resolve(data);
-            })
-            .fail(function (data) {
-              reject(data.responseJSON);
-            });
+          entityType = JSON.stringify(entity);
         }
-      });      
+
+        switch(entityType) {
+          default:
+            switch(action) {
+              default:                
+                resolve( {
+                  sort_name: "Steigerwald, Michael",
+                  display_name:"Mr. Michael Steigerwald",
+                  last_name:"Steigerwald",
+                  first_name:"Michael",
+                  middle_name:"Francis",
+                  birth_date: new Date(1961, 2, 4),
+                  modified_date: new Date(),
+                  id: params.id
+              })
+            }
+        }
+
+      })
         
-      // CRM.api3 returns a promise, but the promise doesn't really represent errors as errors, so we
-      // convert them
-      p.then(
-        function(result) {
-          if (result.is_error) {
-            deferred.reject(result);
-          } else {
-            deferred.resolve(result);
-          }
-        },
-        function(error) {
-          deferred.reject(error);
-        }
-      );
-      return deferred.promise;
     };
-
-    crmApi.backend = null;
-    crmApi.val = function(value) {
-        var d = $.Deferred();
-        d.resolve(value);
-        return d.promise();
-    };
-
-    return crmApi;
 });
