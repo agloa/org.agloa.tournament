5b57a07b8406244d7ef8624b221f01a2ef1d77e5
Mike Steigerwald <steimik@chrobinson.com>
2020-12-31 18:05:35 -0600

    test email.save

--- ang/tournament/core/services/email.service.spec.js
+++ ang/tournament/core/services/email.service.spec.js
@@ -47,6 +47,30 @@ describe('email', function () {
       "return": ["id", "contact_id", "location_type_id", "is_primary", "is_billing", "email"],
       "id": id
     });
+  });  
+
+  it('save should create email in crmApi', () => {
+    const testEmail = {      
+      id: 1
+      , contact_id: 1
+      , location_type_id: 1
+      , is_primary: 1
+      , is_billing: 1
+      , email: "email@test.org"
+    };
+
+    const expectedEmail = testEmail;
+
+    email.save(testEmail);
+
+    expect(crmApi).toHaveBeenCalledWith('Email', 'create', {
+      "id": expectedEmail.id
+      , "contact_id": expectedEmail.contact_id
+      , "location_type_id": expectedEmail.location_type_id
+      , "is_primary": expectedEmail.is_primary
+      , "is_billing": expectedEmail.is_billing
+      , "email": expectedEmail.email
+    });
   });
 
 });
