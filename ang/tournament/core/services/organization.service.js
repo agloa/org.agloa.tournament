ccb9e3263d53b9af9466dd03f5e27018b1e80981
Mike Steigerwald <Steiger@UMich.edu>
2021-03-27 19:50:49 -0500

    save organization with address, phone and email

--- ang/tournament/core/services/organization.service.js
+++ ang/tournament/core/services/organization.service.js
@@ -26,9 +26,22 @@ angular.module('tournament').factory('organization', function (crmApi) {
             }
 
             return crmApi('Contact', 'create', {
-                'id': organization.id,
-                'organization_name': organization.organization_name,
-                'contact_type': organization.contact_type
+                id: organization.id,
+                contact_type: organization.contact_type,
+                contact_sub_type: organization.contact_sub_type,
+                organization_name: organization.organization_name,
+                modified_date: organization.modified_date,
+                email: organization.email,
+                phone: organization.phone,
+                street_address: organization.street_address,
+                supplemental_address_1: organization.supplemental_address_1,
+                supplemental_address_2: organization.supplemental_address_2,
+                supplemental_address_3: organization.supplemental_address_3,
+                city: organization.city,
+                postal_code: organization.postal_code,
+                postal_code_suffix: organization.postal_code_suffix,
+                country_id: organization.country_id,
+                state_province_id: organization.state_province_id
             });
         },
         delete: (id) => {
