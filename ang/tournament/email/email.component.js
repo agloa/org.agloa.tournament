f982ed5ffdbf8246ad38cc905dbb734bdbb1df04
Mike Steigerwald <Steiger@UMich.edu>
2021-04-04 18:31:38 -0500

    add onInit, delete functions

--- ang/tournament/email/email.component.js
+++ ang/tournament/email/email.component.js
@@ -8,20 +8,13 @@ angular.module('tournament').component('trnEmail', {
     function ($routeParams, email) {
       var self = this;
 
-      if ($routeParams.id !== undefined) {
-        self.id = $routeParams.id;
+      if ($routeParams.emailId !== undefined) {
+        self.id = $routeParams.emailId;
       }
 
       if ($routeParams.contactId !== undefined) {
         self.contactId = $routeParams.contactId;
       }
-
-      email.get(self.contactId, self.id).then(
-        // Success
-        (result) => { self.setSelectedEmail(result.values[0]); },
-        // Failure
-        (error) => { CRM.alert(ts('Could not get email record, error = ' + error.error_message), ts('Error'), 'error'); }
-      );
 
       self.setSelectedEmail = (email) => {
         self.id = email.id;
@@ -40,6 +33,26 @@ angular.module('tournament').component('trnEmail', {
           (error) => { CRM.alert(ts('Could not save email record ID = ' + self.selectedEmail.id + ' error = ' + error.error_message), ts('Database error'), 'error'); }
         );
       }
+
+      self.delete = () => {
+        email.delete(self.id).then(
+          // Success
+          () => { CRM.alert(ts("Deleted"), ts("Deleted"), 'info'); },
+          // Failure
+          (error) => { CRM.alert(ts('Could not delete email record ID = ' + self.id + ', error = ' + error.error_message), ts('Database Error'), 'error'); }
+        )
+      };
+
+      self.$onInit = function () {
+        self.id = self.contactId;
+
+        email.get(self.contactId, self.id).then(
+          // Success
+          (result) => { self.setSelectedEmail(result.values[0]); },
+          // Failure
+          (error) => { CRM.alert(ts('Could not get email record, error = ' + error.error_message), ts('Error'), 'error'); }
+        );
+      };
     }
   ]
 });
