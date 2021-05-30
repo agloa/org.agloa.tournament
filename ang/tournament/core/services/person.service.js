51d97a2111a09a0209c17fe685ee4b43572659c3
Mike Steigerwald <CIO@AGLOA.org>
2021-05-30 12:34:01 -0700

    Convert get to api4

--- ang/tournament/core/services/person.service.js
+++ ang/tournament/core/services/person.service.js
@@ -5,10 +5,16 @@ angular.module('tournament').factory('person', function (crmApi) {
     // @return Promise of Contact (per APIv3)
     get: (id) => {
       if (id) {
-        return crmApi('Contact', 'getsingle', {
+          return CRM.api4('Contact', 'get', {
+select: ["display_name","first_name", "last_name", "middle_name", "prefix_id", "suffix_id", "gender_id", "birth_date", "modified_date", "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"],
+  join: [["Address AS address", false, null]],  
+  where: [["id", "=", id]]
+})
+        //return crmApi('Contact', 'getsingle', {
           //"return": ["id", "contact_sub_type", "display_name", "first_name", "middle_name", "last_name", "prefix_id", "suffix_id", "gender_id", "birth_date", "modified_date", "email", "phone", "street_address", "supplemental_address_1", "supplemental_address_2", "supplemental_address_3", "city", "postal_code", "postal_code_suffix", "country_id", "state_province_id"],
-          "id": id
-        });
+          //"id": id
+        //})
+        ;
       }
       return crmApi('Contact', 'get', {
         "return": ["id", "contact_sub_type", "display_name", "first_name", "middle_name", "last_name", "prefix_id", "suffix_id", "gender_id", "birth_date", "modified_date", "email", "phone", "street_address", "supplemental_address_1", "supplemental_address_2", "supplemental_address_3", "city", "postal_code", "postal_code_suffix", "country_id", "state_province_id"],
@@ -18,31 +24,40 @@ angular.module('tournament').factory('person', function (crmApi) {
     // Get values/labels for option groups: gender
     // @return Promise of option groups/values (per APIv3)
     getGenders: () => {
-      return crmApi('OptionValue', 'get', {
-        "sequential": 1,
-        "return": ["value", "label"],
-        "option_group_id": "gender",
-        "is_active": 1
+        return CRM.api4('OptionValue', 'get', {
+  select: ["value", "label"],
+  where: [["option_group_id:name", "=", "gender"], ["is_active", "=", true]]
+      //return crmApi('OptionValue', 'get', {
+        //"sequential": 1,
+        //"return": ["value", "label"],
+        //"option_group_id": "gender",
+        //"is_active": 1
       })
     },
     // Get values/labels for option groups: prefix
     // @return Promise of option groups/values (per APIv3)
     getPrefixes: () => {
-      return crmApi('OptionValue', 'get', {
-        "sequential": 1,
-        "return": ["value", "label"],
-        "option_group_id": "individual_prefix",
-        "is_active": 1
+        return CRM.api4('OptionValue', 'get', {
+  select: ["value", "label"],
+  where: [["option_group_id:name", "=", "individual_prefix"], ["is_active", "=", true]],
+      // return crmApi('OptionValue', 'get', {
+      //   "sequential": 1,
+      //   "return": ["value", "label"],
+      //   "option_group_id": "individual_prefix",
+      //   "is_active": 1
       })
     },
     // Get values/labels for option groups: prefix
     // @return Promise of option groups/values (per APIv3)
-    getSuffixes: () => {
-      return crmApi('OptionValue', 'get', {
-        "sequential": 1,
-        "return": ["value", "label"],
-        "option_group_id": "individual_suffix",
-        "is_active": 1
+    getSuffixes: () => {      
+        return CRM.api4('OptionValue', 'get', {
+  select: ["value", "label"],
+  where: [["option_group_id:name", "=", "individual_suffix"], ["is_active", "=", true]],
+      // return crmApi('OptionValue', 'get', {
+      //   "sequential": 1,
+      //   "return": ["value", "label"],
+      //   "option_group_id": "individual_suffix",
+      //   "is_active": 1
       })
     },
 
