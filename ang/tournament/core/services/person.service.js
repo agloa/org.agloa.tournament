38284c585bc807f23cf527d295d9b9f5f63baf1b
Mike Steigerwald <Steiger@UMich.edu>
2021-06-06 14:11:58 -0500

    Encapsulate crmApi for crmApi4.

--- ang/tournament/core/services/person.service.js
+++ ang/tournament/core/services/person.service.js
@@ -5,16 +5,16 @@ angular.module('tournament').factory('person', function (crmApi, crmApi4) {
     // @return Promise of Contact (per APIv3)
     get: (id) => {
       if (id) {
-          return crmApi4('Contact', 'get', {
-select: ["display_name","first_name", "last_name", "middle_name", "prefix_id", "suffix_id", "gender_id", "birth_date", "modified_date", "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"],
-  join: [["Address AS address", false, null]],  
-  where: [["id", "=", id]]
-})
-        //return crmApi('Contact', 'getsingle', {
+        return crmApi4('Contact', 'get', {
+          select: ["display_name", "first_name", "last_name", "middle_name", "prefix_id", "suffix_id", "gender_id", "birth_date", "modified_date", "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"],
+          join: [["Address AS address", false, null]],
+          where: [["id", "=", id]]
+        })
+          //return crmApi('Contact', 'getsingle', {
           //"return": ["id", "contact_sub_type", "display_name", "first_name", "middle_name", "last_name", "prefix_id", "suffix_id", "gender_id", "birth_date", "modified_date", "email", "phone", "street_address", "supplemental_address_1", "supplemental_address_2", "supplemental_address_3", "city", "postal_code", "postal_code_suffix", "country_id", "state_province_id"],
           //"id": id
-        //})
-        ;
+          //})
+          ;
       }
       return crmApi('Contact', 'get', {
         "return": ["id", "contact_sub_type", "display_name", "first_name", "middle_name", "last_name", "prefix_id", "suffix_id", "gender_id", "birth_date", "modified_date", "email", "phone", "street_address", "supplemental_address_1", "supplemental_address_2", "supplemental_address_3", "city", "postal_code", "postal_code_suffix", "country_id", "state_province_id"],
@@ -24,10 +24,10 @@ select: ["display_name","first_name", "last_name", "middle_name", "prefix_id", "
     // Get values/labels for option groups: gender
     // @return Promise of option groups/values (per APIv3)
     getGenders: () => {
-        return CRM.api4('OptionValue', 'get', {
-  select: ["value", "label"],
-  where: [["option_group_id:name", "=", "gender"], ["is_active", "=", true]]
-      //return crmApi('OptionValue', 'get', {
+      return crmApi4('OptionValue', 'get', {
+        select: ["value", "label"],
+        where: [["option_group_id:name", "=", "gender"], ["is_active", "=", true]]
+        //return crmApi('OptionValue', 'get', {
         //"sequential": 1,
         //"return": ["value", "label"],
         //"option_group_id": "gender",
@@ -37,27 +37,27 @@ select: ["display_name","first_name", "last_name", "middle_name", "prefix_id", "
     // Get values/labels for option groups: prefix
     // @return Promise of option groups/values (per APIv3)
     getPrefixes: () => {
-        return CRM.api4('OptionValue', 'get', {
-  select: ["value", "label"],
-  where: [["option_group_id:name", "=", "individual_prefix"], ["is_active", "=", true]],
-      // return crmApi('OptionValue', 'get', {
-      //   "sequential": 1,
-      //   "return": ["value", "label"],
-      //   "option_group_id": "individual_prefix",
-      //   "is_active": 1
+      return crmApi4('OptionValue', 'get', {
+        select: ["value", "label"],
+        where: [["option_group_id:name", "=", "individual_prefix"], ["is_active", "=", true]],
+        // return crmApi('OptionValue', 'get', {
+        //   "sequential": 1,
+        //   "return": ["value", "label"],
+        //   "option_group_id": "individual_prefix",
+        //   "is_active": 1
       })
     },
     // Get values/labels for option groups: prefix
     // @return Promise of option groups/values (per APIv3)
-    getSuffixes: () => {      
-        return CRM.api4('OptionValue', 'get', {
-  select: ["value", "label"],
-  where: [["option_group_id:name", "=", "individual_suffix"], ["is_active", "=", true]],
-      // return crmApi('OptionValue', 'get', {
-      //   "sequential": 1,
-      //   "return": ["value", "label"],
-      //   "option_group_id": "individual_suffix",
-      //   "is_active": 1
+    getSuffixes: () => {
+      return crmApi4('OptionValue', 'get', {
+        select: ["value", "label"],
+        where: [["option_group_id:name", "=", "individual_suffix"], ["is_active", "=", true]],
+        // return crmApi('OptionValue', 'get', {
+        //   "sequential": 1,
+        //   "return": ["value", "label"],
+        //   "option_group_id": "individual_suffix",
+        //   "is_active": 1
       })
     },
 
