bd7f6ba7dbc3a71e12052c829aaffdd31d728837
Mike Steigerwald <CIO@AGLOA.org>
2021-06-18 15:55:20 -0700

    Use crmApi4 for all of person service.

--- ang/tournament/core/services/person.service.js
+++ ang/tournament/core/services/person.service.js
@@ -6,7 +6,8 @@ angular.module('tournament').factory('person', function (crmApi, crmApi4) {
           select: ["display_name", "first_name", "last_name", "middle_name", "prefix_id", "suffix_id", "gender_id", "birth_date", "modified_date",
             "email.email",
             "phone.phone",
-            "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"
+            "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", 
+            "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"
           ],
           join: [
               ["Email AS email", false, null],          
@@ -15,48 +16,26 @@ angular.module('tournament').factory('person', function (crmApi, crmApi4) {
           ],
           where: [["id", "=", id]]
         });
-      return crmApi('Contact', 'get', {
-        "return": ["id", "contact_sub_type", "display_name", "first_name", "middle_name", "last_name", "prefix_id", "suffix_id", "gender_id", "birth_date", "modified_date", "email", "phone", "street_address", "supplemental_address_1", "supplemental_address_2", "supplemental_address_3", "city", "postal_code", "postal_code_suffix", "country_id", "state_province_id"],
-        "contact_type": "Individual",
-      });
     },
     // Get values/labels for option groups: gender
-    // @return Promise of option groups/values (per APIv3)
     getGenders: () => {
       return crmApi4('OptionValue', 'get', {
         select: ["value", "label"],
         where: [["option_group_id:name", "=", "gender"], ["is_active", "=", true]]
-        //return crmApi('OptionValue', 'get', {
-        //"sequential": 1,
-        //"return": ["value", "label"],
-        //"option_group_id": "gender",
-        //"is_active": 1
       })
     },
     // Get values/labels for option groups: prefix
-    // @return Promise of option groups/values (per APIv3)
     getPrefixes: () => {
       return crmApi4('OptionValue', 'get', {
         select: ["value", "label"],
         where: [["option_group_id:name", "=", "individual_prefix"], ["is_active", "=", true]],
-        // return crmApi('OptionValue', 'get', {
-        //   "sequential": 1,
-        //   "return": ["value", "label"],
-        //   "option_group_id": "individual_prefix",
-        //   "is_active": 1
       })
     },
-    // Get values/labels for option groups: prefix
-    // @return Promise of option groups/values (per APIv3)
+    // Get values/labels for option groups: suffix
     getSuffixes: () => {
       return crmApi4('OptionValue', 'get', {
         select: ["value", "label"],
         where: [["option_group_id:name", "=", "individual_suffix"], ["is_active", "=", true]],
-        // return crmApi('OptionValue', 'get', {
-        //   "sequential": 1,
-        //   "return": ["value", "label"],
-        //   "option_group_id": "individual_suffix",
-        //   "is_active": 1
       })
     },
 
@@ -65,36 +44,19 @@ angular.module('tournament').factory('person', function (crmApi, crmApi4) {
         person.contact_type = "Individual";
       }
 
-      return crmApi('Contact', 'create', {
-        id: person.id,
-        contact_type: person.contact_type,
-        contact_sub_type: person.contact_sub_type,
-        first_name: person.first_name,
-        middle_name: person.middle_name,
-        last_name: person.last_name,
-        prefix_id: person.prefix_id,
-        suffix_id: person.suffix_id,
-        gender_id: person.gender_id,
-        birth_date: person.birth_date,
-        modified_date: person.modified_date,
-        "api.Email.create": { email: person.email },
-        "api.Phone.create": { phone: person.phone },
-        "api.Address.create": {
-          street_address: person.street_address,
-          supplemental_address_1: person.supplemental_address_1,
-          supplemental_address_2: person.supplemental_address_2,
-          supplemental_address_3: person.supplemental_address_3,
-          city: person.city,
-          postal_code: person.postal_code,
-          postal_code_suffix: person.postal_code_suffix,
-          country_id: person.country_id,
-          state_province_id: person.state_province_id
-        }
-      });
+      return crmApi4({results: ['Contact', 'save', {records: [{"id":person.id, 
+      "first_name":person.first_name, "last_name":person.last_name, "middle_name":person.middle_name, 
+      "prefix_id":person.prefix_id, "suffix_id":person.suffix_id, "gender_id":person.gender_id, "birth_date":person.birth_date, 
+      "address.street_address":person.street_address, 
+      "address.supplemental_address_1":person.supplemental_address_1,  "address.supplemental_address_2":person.supplemental_address_2, "address.supplemental_address_3":person.supplemental_address_3, 
+      "address.city":person.city, "address.state_province_id":person.state_province_id, "address.country_id":person.country_id, 
+      "address.postal_code":person.postal_code, "address.postal_code_suffix":person.postal_code_suffix, 
+      "email.email":person.email, "phone.phone":person.phone}],
+      }]});
     },
 
     delete: (id) => {
-      return crmApi('Contact', 'delete', { id });
+        return crmApi4('Contact', 'delete', {where: [["id", "=", id]]});
     }
   }
 });
