a64078e5d8d35de768113c045437242b20d1217f
Mike Steigerwald <Steiger@UMich.edu>
2021-07-19 19:49:16 -0500

    chain org save

--- ang/tournament/core/services/organization.service.js
+++ ang/tournament/core/services/organization.service.js
@@ -22,28 +22,28 @@ angular.module('tournament').factory('organization', function (crmApi, crmApi4) 
                 organization.contact_type = "Organization";
             }
 
-            return crmApi4({
-                results: ['Contact', 'save', {
-                    records: [{
-                        "id": organization.id,
-                        "contact_type": organization.contact_type,
-                        "contact_sub_type": organization.contact_sub_type,
-                        "organization_name": organization.organization_name,
-                        "modified_date": organization.modified_date,
-                        "email": organization.email,
-                        "phone": organization.phone,
-                        "street_address": organization.street_address,
-                        "supplemental_address_1": organization.supplemental_address_1,
-                        "supplemental_address_2": organization.supplemental_address_2,
-                        "supplemental_address_3": organization.supplemental_address_3,
-                        "city": organization.city,
-                        "postal_code": organization.postal_code,
-                        "postal_code_suffix": organization.postal_code_suffix,
-                        "country_id": organization.country_id,
-                        "state_province_id": organization.state_province_id
-                    }],
-                }]
-            });
+            return crmApi4('Contact', 'save', {
+                records: [{
+                    "id": organization.id,
+                    "contact_type": organization.contact_type,
+                    "contact_sub_type": organization.contact_sub_type,
+                    "organization_name": organization.organization_name,
+                }],
+                chain: {
+                    "emailSave": ["Email", "save", { "records": [{ "contact_id": organization.id, "id": organization.emailId, "email": organization.email }] }],
+                    "phoneSave": ["Phone", "save", { "records": [{ "contact_id": organization.id, "id": organization.phoneId, "phone": organization.phone }] }],
+                    "addressSave": ["Address", "save", {
+                        "records": [{
+                            "contact_id": organization.id, "id": organization.addressId,
+                            "street_address": organization.street_address,
+                            "supplemental_address_1": organization.supplemental_address_1, "supplemental_address_2": organization.supplemental_address_2, "supplemental_address_3": organization.supplemental_address_3,
+                            "city": organization.city, "state_province_id": organization.state_province_id, "country_id": organization.country_id,
+                            "postal_code": organization.postal_code, "postal_code_suffix": organization.postal_code_suffix
+                        }]
+                    }]
+
+                }
+            })
         },
 
         delete: (id) => {
