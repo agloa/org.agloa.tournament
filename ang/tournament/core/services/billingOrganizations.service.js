39927e930f652e0f91948b90d536a3cb9549887e
Mike Steigerwald <Steiger@UMich.edu>
2021-11-08 08:52:18 -0600

    convert billingOrganizations to crmApi4

--- ang/tournament/core/services/billingOrganizations.service.js
+++ ang/tournament/core/services/billingOrganizations.service.js
@@ -1,23 +1,16 @@
-angular.module('tournament').factory('billingOrganizations', function (crmApi) {
-
+angular.module('tournament').factory('billingOrganizations', function (crmApi4) {
     function getContactType() {
-        const params = {
-            "label": "Billing Organization",
-            "parent_id": "Organization",
-            "is_active": 1
-        };
-
-        crmApi('ContactType', 'get', params).then(
+        crmApi4('ContactType', 'get', {
+            select: ["id"],
+            where: [["label", "=", "Billing Organization"], ["parent_id:name", "=", "Organization"], ["is_active", "=", true]],
+            limit: 25
+        }).then(
             (result) => {
                 if (result.count > 0) {
                     return result.id;
                 } else {
-                    crmApi('ContactType', 'create', {
-                        "name": "billingOrganization",
-                        "label": "Billing Organization",
-                        "description": "Organization that will pay, e.g., registration fees",
-                        "parent_id": "Organization",
-                        "is_active": 1
+                    crmApi4('ContactType', 'create', {
+                        values: { "name": "billingOrganization", "label": "Billing Organization", "description": "Organization that will pay,  e.g.,  registration fees", "parent_id:name": "Organization", "is_active": true }
                     }).then(
                         (contactType) => {
                             return contactType.id;
@@ -36,32 +29,61 @@ angular.module('tournament').factory('billingOrganizations', function (crmApi) {
 
     return {
         get: (id) => {
-            crmApi4('Contact', 'get', {
-                select: ["id", "organization_name"],
-                where: [["contact_type", "=", "Organization"], ["contact_sub_type", "=", "billingOrganization"]]
+            let wherClause = (id) ? [
+                ["contact_type", "=", "Organization"],
+                ["contact_sub_type", "=", "billingOrganization"],
+                ["id", "=", id]
+            ]
+                : [["contact_type", "=", "Organization"], ["contact_sub_type", "=", "billingOrganization"]];
+
+            return crmApi4('Contact', 'get', {
+                select: ["id", "contact_sub_type", "organization_name", "modified_date",
+                    "email.id", "email.email",
+                    "phone.id", "phone.phone",
+                    "address.id", "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"
+                ],
+                join: [
+                    ["Email AS email", false, null],
+                    ["Phone AS phone", false, null],
+                    ["Address AS address", false, null]
+                ],
+                where: wherClause
+            });
+        },
+        save: (organization) => {
+            if (!organization.contact_type) {
+                organization.contact_type = "Organization";
+            }
+
+            if (!organization.contact_sub_type) {
+                organization.contact_sub_type = getContactType();
+            }
+
+            return crmApi4('Contact', 'save', {
+                records: [{
+                    "id": organization.id,
+                    "contact_type": organization.contact_type,
+                    "contact_sub_type": organization.contact_sub_type,
+                    "organization_name": organization.organization_name,
+                }],
+                chain: {
+                    "emailSave": ["Email", "save", { "records": [{ "contact_id": organization.id, "id": organization.emailId, "email": organization.email }] }],
+                    "phoneSave": ["Phone", "save", { "records": [{ "contact_id": organization.id, "id": organization.phoneId, "phone": organization.phone }] }],
+                    "addressSave": ["Address", "save", {
+                        "records": [{
+                            "contact_id": organization.id, "id": organization.addressId,
+                            "street_address": organization.street_address,
+                            "supplemental_address_1": organization.supplemental_address_1, "supplemental_address_2": organization.supplemental_address_2, "supplemental_address_3": organization.supplemental_address_3,
+                            "city": organization.city, "state_province_id": organization.state_province_id, "country_id": organization.country_id,
+                            "postal_code": organization.postal_code, "postal_code_suffix": organization.postal_code_suffix
+                        }]
+                    }]
+
+                }
             })
         },
-        save: (organizationName) => {
-            ('Contact', 'create', {
-                "contact_type": "Organization",
-                "contact_sub_type": getContactType(),
-                "organization_name": organizationName
-            }).then(
-                (result) => {
-                    return result.id;
-                }, (error) => {
-                    CRM.alert(ts('Could not save Billing Organization ' + organizationName +
-                        ' , error = ' + error.error_message), ts('Error'), 'error');
-                });
-        },
         delete: (id) => {
-            crmApi('Contact', 'delete', { id }).then(
-                (result) => {
-                    return result.id;
-                }, (error) => {
-                    CRM.alert(ts('Could not delete Billing Organization id = ' + id +
-                        ' , error = ' + error.error_message), ts('Error'), 'error');
-                });
+            return crmApi4('Contact', 'delete', { where: [["id", "=", id]] });
         }
     }
 });
