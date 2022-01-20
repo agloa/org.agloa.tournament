0071f650f4d57ca344be5908cb2018fe58ca5c59
Mike Steigerwald <Steiger@UMich.edu>
2022-01-19 20:01:25 -0600

    finish update to crmApi4

--- ang/tournament/core/services/billingRelationship.service.js
+++ ang/tournament/core/services/billingRelationship.service.js
@@ -1,12 +1,12 @@
 angular.module('tournament').factory('billingRelationship', function (crmApi4) {
     function getRelationshipType() {
-        crmApi4('RelationshipType', 'get', {
+        return crmApi4('RelationshipType', 'get', {
             select: ["id"],
             where: [["contact_type_a", "=", "Individual"], ["contact_type_b", "=", "Organization"], ["is_active", "=", true], ["description", "LIKE", "%Billing contact relationship%"]],
         }).then(
-            (relationshipType) => {
-                if (relationshipType.count > 0) {
-                    return relationshipType.id;
+            (result) => {
+                if (result.length > 0) {
+                    return result[0].id;
                 } else {
                     crmApi4('RelationshipType', 'create', {
                         values: { "name_a_b": "billingContactFor", "name_b_a": "billingContactIs", "label_a_b": "Billing contact for", "label_b_a": "Billing contact is", "description": "Billing contact relationship", "contact_type_a": "Individual", "contact_type_b": "Organization", "is_active": true }
@@ -19,6 +19,27 @@ angular.module('tournament').factory('billingRelationship', function (crmApi4) {
             }
         )
     };
+
+    function save(crmApi4, relationship) {
+        crmApi4("Relationship", "save", {
+            records: [
+                {
+                    id: relationship.id,
+                    contact_id_a: relationship.contact_id_a,
+                    contact_id_b: relationship.contact_id_b,
+                    relationship_type_id: relationship.relationship_type_id,
+                    start_date: relationship.start_date,
+                    end_date: relationship.end_date,
+                    is_active: relationship.is_active,
+                    description: relationship.description,
+                    is_permission_a_b: relationship.is_permission_a_b,
+                    is_permission_b_a: relationship.is_permission_b_a,
+                },
+            ],
+        }).then((result) => {
+            return result.id;
+        });
+    }
 
     return {
         get: (contact_id) => {
@@ -36,10 +57,6 @@ angular.module('tournament').factory('billingRelationship', function (crmApi4) {
             });
         },
         save: (relationship) => {
-            if (!relationship.relationship_type_id) {
-                relationship.relationship_type_id = getRelationshipType();
-            }
-
             if (!relationship.start_date) {
                 relationship.start_date = Date.now();
             }
@@ -64,23 +81,26 @@ angular.module('tournament').factory('billingRelationship', function (crmApi4) {
                     relationship.contact_id_b;
             }
 
-            crmApi4('Relationship', 'save', {
-                records: [{
-                    "id": relationship.id,
-                    "contact_id_a": relationship.contact_id_a,
-                    "contact_id_b": relationship.contact_id_b,
-                    "relationship_type_id": relationship.relationship_type_id,
-                    "start_date": relationship.start_date,
-                    "end_date": relationship.end_date,
-                    "is_active": relationship.is_active,
-                    "description": relationship.description,
-                    "is_permission_a_b": relationship.is_permission_a_b,
-                    "is_permission_b_a": relationship.is_permission_b_a
-                }]
-            }).then(
-                (result) => {
-                    return result.id;
-                });
+            if (!relationship.relationship_type_id) {
+                return getRelationshipType().then(
+                    (result) => {
+                        relationship.relationship_type_id = result;
+                        return save(crmApi4, relationship);
+                    },
+                    (error) => {
+                        CRM.alert(
+                            ts(
+                                "Could not get relationshipe type error = " +
+                                error.error_message
+                            ),
+                            ts("Database error"),
+                            "error"
+                        );
+                    }
+                );
+            } else {
+                return save(crmApi4, relationship);
+            }
         },
         delete: (id) => {
             return crmApi4('Relationship', 'delete', { where: [["id", "=", id]] });
