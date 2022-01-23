cffc9373f2863b05aeabb86f0c2ebfe65120e7d6
Mike Steigerwald <Steiger@UMich.edu>
2022-01-22 18:36:19 -0600

    Rename core/services to simply services

--- ang/tournament.ang.php
+++ ang/tournament.ang.php
@@ -2,21 +2,21 @@
 // This file declares an Angular module which can be autoloaded in CiviCRM. See also:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
 
-return array (
-  'js' => 
-    array (
-        0 => 'ang/app.js',
-        1 => 'ang/tournament/core/services/*.service.js',
-        2 => 'ang/tournament/*/*.js',
-    ),
-  'css' => 
-    array (
-        0 => 'ang/tournament.css',
-    ),
-  'partials' => 
-    array (
-        0 => 'ang/tournament',
-    ),
-  'settings' => 
-    array (),
+return array(
+  'js' =>
+  array(
+    0 => 'ang/app.js',
+    1 => 'ang/tournament/services/*.service.js',
+    2 => 'ang/tournament/*/*.js',
+  ),
+  'css' =>
+  array(
+    0 => 'ang/tournament.css',
+  ),
+  'partials' =>
+  array(
+    0 => 'ang/tournament',
+  ),
+  'settings' =>
+  array(),
 );
