0514f0ad912be2db203b393ae90ee3b89d01726f
Mike Steigerwald <Steiger@UMich.edu>
2022-02-20 17:48:55 -0600

    need to be explicit with "billing organization"

--- ang/tournament/services/billingOrganization.service.js
+++ ang/tournament/services/billingOrganization.service.js
@@ -1,15 +1,16 @@
 angular.module('tournament').factory('billingOrganization', function (crmApi4) {
     function getContactType() {
+        const label = "Billing Organization";
         return crmApi4('ContactType', 'get', {
             select: ["id"],
-            where: [["label", "=", "Billing Organization"], ["parent_id:name", "=", "Organization"], ["is_active", "=", true]],
+            where: [["label", "=", label], ["parent_id:name", "=", "Organization"], ["is_active", "=", true]],
         }).then(
             (result) => {
                 if (result.length > 0) {
                     return result[0].id;
                 } else {
                     crmApi4('ContactType', 'create', {
-                        values: { "name": "billingOrganization", "label": "Billing Organization", "description": "Organization that will pay, e.g.,  registration fees", "parent_id:name": "Organization", "is_active": true }
+                        values: { "name": "billingOrganization", "label": label, "description": "Organization that will pay, e.g.,  registration fees", "parent_id:name": "Organization", "is_active": true }
                     }).then(
                         (contactType) => {
                             return contactType.id;
@@ -40,56 +41,54 @@ angular.module('tournament').factory('billingOrganization', function (crmApi4) {
                         "postal_code": organization.postal_code, "postal_code_suffix": organization.postal_code_suffix
                     }]
                 }]
-
             }
         })
     }
 
     return {
         get: (id) => {
-            let wherClause = (id) ? [
-                ["contact_type", "=", "Organization"],
-                ["contact_sub_type", "=", "billingOrganization"],
-                ["id", "=", id]
-            ]
-                : [["contact_type", "=", "Organization"], ["contact_sub_type", "=", "billingOrganization"]];
-
-            return crmApi4('Contact', 'get', {
-                select: ["id", "contact_sub_type", "organization_name", "modified_date",
-                    "email.id", "email.email",
-                    "phone.id", "phone.phone",
-                    "address.id", "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"
-                ],
-                join: [
-                    ["Email AS email", false, null],
-                    ["Phone AS phone", false, null],
-                    ["Address AS address", false, null]
-                ],
-                where: wherClause
-            });
+            return getContactType().then(
+                (contactType) => {
+                    return crmApi4('Contact', 'get', {
+                        select: ["id", "contact_sub_type", "organization_name", "modified_date",
+                            "email.id", "email.email",
+                            "phone.id", "phone.phone",
+                            "address.id", "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"
+                        ],
+                        join: [
+                            ["Email AS email", false, null],
+                            ["Phone AS phone", false, null],
+                            ["Address AS address", false, null]
+                        ],
+                        where: (id) ? [["id", "=", id]] : [["contact_type", "=", contactType]]
+                    });
+                },
+                (error) => {
+                    CRM.alert(
+                        ts(`Could not get relationshipe type. Error message: = ${error.error_message}`),
+                        ts("Database error"),
+                        "error"
+                    );
+                }
+            );
         },
         save: (organization) => {
             if (!organization.contact_type) {
-                organization.contact_type = "Organization";
-            }
-
-            if (!organization.contact_sub_type) {
-                return getContactType().then(
-                    (result) => {
-                        organization.contact_sub_type = result;
+                getContactType().then(
+                    (contactType) => {
+                        organization.contact_type = contactType;
                         return save(crmApi4, organization);
                     },
                     (error) => {
                         CRM.alert(
-                            ts("Could not get contact type error = " + error.error_message),
+                            ts(`Could not get relationshipe type. Error message: = ${error.error_message}`),
                             ts("Database error"),
                             "error"
                         );
-                    }
-                );
-            } else {
-                return save(crmApi4, organization);
+                    });
             }
+
+            return save(crmApi4, organization);
         },
         delete: (id) => {
             return crmApi4('Contact', 'delete', { where: [["id", "=", id]] });
--- ang/unit-tests/services/billingOrganization.service.spec.js
+++ ang/unit-tests/services/billingOrganization.service.spec.js
@@ -3,7 +3,7 @@
 describe('Billing Organization service', function () {
   var billingOrganization;
   var CRM;
-  const billingOrganizationContactSubType = 6;
+  const contactType = 6;
 
   beforeEach(function () {
     // Add a custom equality tester before each test
@@ -16,7 +16,7 @@ describe('Billing Organization service', function () {
         resolve(
           [
             {
-              id: billingOrganizationContactSubType
+              id: contactType
             }
           ]
         )
@@ -30,70 +30,54 @@ describe('Billing Organization service', function () {
     inject(function (_billingOrganization_) { billingOrganization = _billingOrganization_; });
   });
 
-  it('gets all contacts from CRM.', function () {
-    billingOrganization.get();
+  it('gets all billing organizations from CRM.', async function () {
+    await billingOrganization.get();
     expect(CRM).toHaveBeenCalledWith('Contact', 'get', {
-      select: ['id', 'contact_sub_type', 'organization_name', 'modified_date', 'email.id', 'email.email', 'phone.id', 'phone.phone', 'address.id', 'address.street_address', 'address.supplemental_address_1', 'address.supplemental_address_2', 'address.supplemental_address_3', 'address.city', 'address.state_province_id', 'address.country_id', 'address.postal_code', 'address.postal_code_suffix'],
-      join: [['Email AS email', false, null], ['Phone AS phone', false, null], ['Address AS address', false, null]],
-      where: [['contact_type', '=', 'Organization'], ['contact_sub_type', '=', 'billingOrganization']]
+      select: ["id", "contact_sub_type", "organization_name", "modified_date",
+        "email.id", "email.email",
+        "phone.id", "phone.phone",
+        "address.id", "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"
+      ],
+      join: [
+        ["Email AS email", false, null],
+        ["Phone AS phone", false, null],
+        ["Address AS address", false, null]
+      ],
+      where: [["contact_type", "=", contactType]]
     });
   });
 
-  it('gets contact by id from CRM.', function () {
-    const id = 6;
-    billingOrganization.get(id);
+  it('gets contact by id from CRM.', async function () {
+    const id = 1;
+    await billingOrganization.get(id);
     expect(CRM).toHaveBeenCalledWith('Contact', 'get', {
-      select: ['id', 'contact_sub_type', 'organization_name', 'modified_date', 'email.id', 'email.email', 'phone.id', 'phone.phone', 'address.id', 'address.street_address', 'address.supplemental_address_1', 'address.supplemental_address_2', 'address.supplemental_address_3', 'address.city', 'address.state_province_id', 'address.country_id', 'address.postal_code', 'address.postal_code_suffix'],
-      join: [['Email AS email', false, null], ['Phone AS phone', false, null], ['Address AS address', false, null]],
-      where: [['contact_type', '=', 'Organization'], ['contact_sub_type', '=', 'billingOrganization'], ['id', '=', 6]]
+      select: ["id", "contact_sub_type", "organization_name", "modified_date",
+        "email.id", "email.email",
+        "phone.id", "phone.phone",
+        "address.id", "address.street_address", "address.supplemental_address_1", "address.supplemental_address_2", "address.supplemental_address_3", "address.city", "address.state_province_id", "address.country_id", "address.postal_code", "address.postal_code_suffix"
+      ],
+      join: [
+        ["Email AS email", false, null],
+        ["Phone AS phone", false, null],
+        ["Address AS address", false, null]
+      ],
+      where: [["id", "=", id]]
     });
   });
 
-  it('saves contact to CRM.', () => {
+  it('saves contact to CRM.', async function () {
     const testOrganization = {
       id: 1,
       organization_name: "organization_name",
-      contact_sub_type: billingOrganizationContactSubType
-    };
-
-    billingOrganization.save(testOrganization);
-    expect(CRM).toHaveBeenCalledWith('Contact', 'save', {
-      records: [{
-        "id": testOrganization.id,
-        "contact_type": testOrganization.contact_type,
-        "contact_sub_type": testOrganization.contact_sub_type,
-        "organization_name": testOrganization.organization_name,
-      }],
-      chain: {
-        "emailSave": ["Email", "save", { "records": [{ "contact_id": testOrganization.id, "id": testOrganization.emailId, "email": testOrganization.email }] }],
-        "phoneSave": ["Phone", "save", { "records": [{ "contact_id": testOrganization.id, "id": testOrganization.phoneId, "phone": testOrganization.phone }] }],
-        "addressSave": ["Address", "save", {
-          "records": [{
-            "contact_id": testOrganization.id, "id": testOrganization.addressId,
-            "street_address": testOrganization.street_address,
-            "supplemental_address_1": testOrganization.supplemental_address_1, "supplemental_address_2": testOrganization.supplemental_address_2, "supplemental_address_3": testOrganization.supplemental_address_3,
-            "city": testOrganization.city, "state_province_id": testOrganization.state_province_id, "country_id": testOrganization.country_id,
-            "postal_code": testOrganization.postal_code, "postal_code_suffix": testOrganization.postal_code_suffix
-          }]
-        }]
-
-      }
-    });
-  });
-
-  it('creates contact_sub_type in CRM.', async () => {
-    const testOrganization = {
-      id: 1,
-      organization_name: "organization_name",
+      contact_type: contactType
     };
 
     await billingOrganization.save(testOrganization);
     expect(CRM).toHaveBeenCalledWith('Contact', 'save', {
       records: [{
         "id": testOrganization.id,
+        "contact_type": testOrganization.contact_type,
         "organization_name": testOrganization.organization_name,
-        "contact_type": "Organization",
-        "contact_sub_type": billingOrganizationContactSubType,
       }],
       chain: {
         "emailSave": ["Email", "save", { "records": [{ "contact_id": testOrganization.id, "id": testOrganization.emailId, "email": testOrganization.email }] }],
@@ -107,14 +91,43 @@ describe('Billing Organization service', function () {
             "postal_code": testOrganization.postal_code, "postal_code_suffix": testOrganization.postal_code_suffix
           }]
         }]
-
       }
     });
   });
 
-  it('deletes organization from CRM.', () => {
-    const id = 6;
-    billingOrganization.delete(id);
+  it('creates contact_type in CRM.', async function () {
+    const organization = {
+      id: 1,
+      organization_name: "organization_name",
+    };
+
+    await billingOrganization.save(organization);
+    expect(CRM).toHaveBeenCalledWith('Contact', 'save', {
+      records: [{
+        "id": organization.id,
+        "contact_type": organization.contact_type,
+        "contact_sub_type": organization.contact_sub_type,
+        "organization_name": organization.organization_name,
+      }],
+      chain: {
+        "emailSave": ["Email", "save", { "records": [{ "contact_id": organization.id, "id": organization.emailId, "email": organization.email }] }],
+        "phoneSave": ["Phone", "save", { "records": [{ "contact_id": organization.id, "id": organization.phoneId, "phone": organization.phone }] }],
+        "addressSave": ["Address", "save", {
+          "records": [{
+            "contact_id": organization.id, "id": organization.addressId,
+            "street_address": organization.street_address,
+            "supplemental_address_1": organization.supplemental_address_1, "supplemental_address_2": organization.supplemental_address_2, "supplemental_address_3": organization.supplemental_address_3,
+            "city": organization.city, "state_province_id": organization.state_province_id, "country_id": organization.country_id,
+            "postal_code": organization.postal_code, "postal_code_suffix": organization.postal_code_suffix
+          }]
+        }]
+      }
+    });
+  });
+
+  it('deletes organization from CRM.', async function () {
+    const id = 1;
+    await billingOrganization.delete(id);
     expect(CRM).toHaveBeenCalledWith('Contact', 'delete', { where: [["id", "=", id]] });
   });
 
