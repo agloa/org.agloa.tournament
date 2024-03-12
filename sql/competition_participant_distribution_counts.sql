2da3740d6801492ef0dfe5710ecef832989f0b59
agloacio <131702060+agloacio@users.noreply.github.com>
2024-03-12 07:21:54 -0500

    Update competition_participant_distribution_counts.sql with names instead of codes

--- sql/competition_participant_distribution_counts.sql
+++ sql/competition_participant_distribution_counts.sql
@@ -5,12 +5,12 @@ CREATE
 VIEW `competition_participant_distribution_counts` AS
     SELECT 
         `competition_participant_distribution`.`competition_id` AS `competition_id`,
-        `competition_participant_distribution`.`registration_group_code` AS `registration_group_code`,
-        COUNT(`competition_participant_distribution`.`registration_group_code`) AS `registration_group_count`,
-        `competition_participant_distribution`.`scheduling_group_code` AS `scheduling_group_code`,
-        COUNT(`competition_participant_distribution`.`scheduling_group_code`) AS `scheduling_group_count`,
+        `competition_participant_distribution`.`registration_group_name` AS `registration_group_name`,
+        COUNT(`competition_participant_distribution`.`registration_group_name`) AS `registration_group_count`,
+        `competition_participant_distribution`.`scheduling_group_name` AS `scheduling_group_name`,
+        COUNT(`competition_participant_distribution`.`scheduling_group_name`) AS `scheduling_group_count`,
         `competition_participant_distribution`.`region_code` AS `region_code`,
         COUNT(`competition_participant_distribution`.`region_code`) AS `region_count`
     FROM
         `competition_participant_distribution`
-    GROUP BY `competition_participant_distribution`.`competition_id` , `competition_participant_distribution`.`region_code` , `competition_participant_distribution`.`scheduling_group_code` , `competition_participant_distribution`.`registration_group_code` WITH ROLLUP
+    GROUP BY `competition_participant_distribution`.`competition_id` , `competition_participant_distribution`.`region_code` , `competition_participant_distribution`.`scheduling_group_name` , `competition_participant_distribution`.`registration_group_name` WITH ROLLUP
